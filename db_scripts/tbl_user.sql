57fedde3be48b7d03646f3fd7978a206411744af
lemonprogis <edward.briggler@gmail.com>
2016-10-12 22:17:47 -0500

    db scripts

---
+++ db_scripts/tbl_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `BucketList`.`tbl_user` (   
+	`user_id` BIGINT AUTO_INCREMENT,   
+    `user_name` VARCHAR(255) NULL,   
+    `user_username` VARCHAR(255) NULL,   
+    `user_password` VARCHAR(255) NULL,   
+    PRIMARY KEY (`user_id`));
